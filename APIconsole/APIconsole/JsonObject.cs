5a3bcf772e30ffb91f47c799079eb17b8a0453a7
Mike V <dev.mikev@gmail.com>
2018-11-01 15:49:26 -0400

    learning Csharp API using Newtonsoft

---
+++ APIconsole/APIconsole/JsonObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIconsole
+{
+
+    public class JsonObject
+    {
+        public USD_EUR USD_EUR { get; set; }
+    }
+
+    public class USD_EUR
+    {
+        public float val { get; set; }
+    }
+
+}
---
+++ APIconsole/APIconsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string result = GetRequest("http://free.currencyconverterapi.com/api/v5/convert?q=USD_EUR&compact=y").Result;
+
+            JsonObject output = JsonConvert.DeserializeObject<JsonObject>(result);
+            Console.WriteLine(output.USD_EUR.val);
+
+        }
+
+        async static Task<string> GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Console.WriteLine("client created");
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    Console.WriteLine("response created");
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        //Console.WriteLine(mycontent);
+                        return mycontent;
+                    }
+                }
+
+
+            }
+        }
+    }
+}
